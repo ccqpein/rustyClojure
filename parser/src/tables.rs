87587eb7fa18c53da375fe02d733133e4a726867
ccQpein <ccqpein@protonmail.com>
2019-06-14 15:37:16 -0400

    a bit

--- parser/src/tables.rs
+++ parser/src/tables.rs
@@ -37,6 +37,7 @@ impl Scope {
         let leng = content.len();
 
         //check first token
+        println!("{}, {}, {:?}", leng, id, content);
         if leng < 1 || &content[0] != "(" {
             return Err(Error::new(ErrorKind::InvalidInput, "Wrong input."));
         }
@@ -44,6 +45,13 @@ impl Scope {
         //clean first "("
         content.drain(..1);
         loop {
+            if content.len() == 0 {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    "Found unmatched \"(\" ",
+                ));
+            }
+            println!("{:?}", content);
             match content[0].as_str() {
                 "(" => {
                     result
@@ -55,7 +63,7 @@ impl Scope {
                     continue;
                 }
                 ")" => {
-                    if id == 0 {
+                    if id == 0 && content.len() > 1 {
                         return Err(Error::new(
                             ErrorKind::InvalidInput,
                             "Found unmatched \")\" ",
@@ -84,12 +92,43 @@ impl Scope {
     fn add_to_scope_table<'a, 'b: 'a>(&'b self, target: &mut ScopeTable<'a>) {
         target.insert(self.id, self);
     }
+
+    fn all_children_scopes(&self) -> Vec<&ExpressionNode> {
+        self.expression
+            .iter()
+            .filter(|x| {
+                if let ExpressionNode::Scope(_) = x {
+                    true
+                } else {
+                    false
+                }
+            })
+            .collect::<Vec<_>>()
+    }
 }
 
-fn new_scope_table<'a>(scope: &Scope) -> Result<ScopeTable<'a>> {
+fn new_scope_table<'a>(scope: &'a Scope) -> Result<ScopeTable<'a>> {
     let mut result: ScopeTable<'a> = HashMap::new();
-    let mut this_scope = scope;
-    {}
+    result.insert(scope.id, scope);
+
+    let mut scope_search_stack = scope.all_children_scopes();
+
+    while scope_search_stack.len() != 0 {
+        let first: &Scope = if let Some(f) = scope_search_stack.first() {
+            if let ExpressionNode::Scope(s) = f {
+                s
+            } else {
+                break;
+            }
+        } else {
+            break;
+        };
+        first.add_to_scope_table(&mut result);
+        let mut new_children = first.all_children_scopes();
+        scope_search_stack.append(&mut new_children);
+        scope_search_stack.drain(1..);
+    }
+
     Ok(result)
 }
 
@@ -114,7 +153,7 @@ mod tests {
             String::from(")"),
         ];
 
-        println!("{:#?}", Scope::from_tokens(0, &mut testcase0));
+        //println!("testcase0: {:#?}", Scope::from_tokens(0, &mut testcase0));
 
         //if more ) at endding
         let mut testcase1 = vec![
@@ -132,13 +171,62 @@ mod tests {
             String::from(")"),
         ];
 
-        if let Err(e) = Scope::from_tokens(0, &mut testcase1) {
-            println!("{:?}", e.description());
+        // if let Err(e) = Scope::from_tokens(0, &mut testcase1) {
+        //     println!("testcase1: {:?}", e.description());
+        // } else {
+        //     panic!();
+        // }
+
+        //if more ( at beginning
+        let mut testcase2 = vec![
+            String::from("("),
+            String::from("("),
+            String::from("defun"),
+            String::from("test"),
+            String::from("("),
+            String::from("a"),
+            String::from(")"),
+            String::from("("),
+            String::from("print"),
+            String::from("\"a\""),
+            String::from(")"),
+            String::from(")"),
+            //String::from(")"),
+        ];
+
+        // if let Err(e) = Scope::from_tokens(0, &mut testcase2) {
+        //     println!("testcase2: {:?}", e.description());
+        // } else {
+        //     println!("testcase2: {:?}", Scope::from_tokens(0, &mut testcase2));
+        //     panic!();
+        // }
+
+        // if more ( also more )
+        let mut testcase3 = vec![
+            String::from("("),
+            String::from("("),
+            String::from("defun"),
+            String::from("test"),
+            String::from("("),
+            String::from("a"),
+            String::from(")"),
+            String::from("("),
+            String::from("print"),
+            String::from("\"a\""),
+            String::from(")"),
+            String::from(")"),
+            String::from(")"),
+        ];
+
+        if let Err(e) = Scope::from_tokens(0, &mut testcase3) {
+            println!("testcase3: {:?}", e.description());
         } else {
+            println!("testcase3: {:?}", Scope::from_tokens(0, &mut testcase3));
             panic!();
         }
     }
 
+    //#[test]
     fn scope_table_generate() {
         let mut testcase0 = vec![
             String::from("("),
@@ -154,7 +242,8 @@ mod tests {
             String::from(")"),
         ];
 
-        let a = Scope::from_tokens(0, &mut testcase0).unwrap();
-        let table = a.new_scope_table();
+        let a = Scope::from_tokens(0, &mut testcase0);
+        println!("{:#?}", a);
+        //println!("{:#?}", new_scope_table(&a));
     }
 }
