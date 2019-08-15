3e1d6b7fdaa02a4b6c5d71b2a9aa32b0ef3699f5
ccQpein <ccqpein@protonmail.com>
2019-08-14 22:20:24 -0400

    add a bit

--- generator/src/go_generator.rs
+++ generator/src/go_generator.rs
@@ -5,26 +5,13 @@ use std::collections::HashMap;
 // generator traits
 // generator impl
 
+struct GoTemplate {}
+
+struct GoTemplates {}
+
 struct GoGenerator {
     keywords_and_temp: HashMap<ExpressionNode, String>, //:= TODO: need find template module for this
     user_space_definition: HashMap<ExpressionNode, String>, //:= TODO: maybe not string
-}
-
-//:= TODO: need tamplate engine to finish this
-impl super::Generator for GoGenerator {
-    type Template = String;
-    type Keyword = ExpressionNode;
-
-    type Result = String;
-
-    fn keyword_template(&self, k: &Self::Keyword) -> String {
-        self.keywords_and_temp.get(k).unwrap().to_string()
-    }
-
-    //:= fake implenment
-    fn match_template(&self, t: &String, se: &SExpression) -> String {
-        String::new()
-    }
 }
 
 impl GoGenerator {
@@ -32,3 +19,20 @@ impl GoGenerator {
         String::new()
     }
 }
+
+//:= TODO: need tamplate engine to finish this
+// impl super::Generator for GoGenerator {
+//     type Template = String;
+//     type Keyword = ExpressionNode;
+
+//     type Result = String;
+
+//     fn keyword_template(&self, k: &Self::Keyword) -> String {
+//         self.keywords_and_temp.get(k).unwrap().to_string()
+//     }
+
+//     //:= fake implenment
+//     fn match_template(&self, t: &String, se: &SExpression) -> String {
+//         String::new()
+//     }
+// }
